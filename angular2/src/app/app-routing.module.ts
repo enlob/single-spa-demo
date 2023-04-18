7fa9ccfbcacad52742ddffc40f775900510e3c3d
enlob <enricolobianco@hotmail.it>
2023-04-18 22:38:18 +0200

    base href

--- angular2/src/app/app-routing.module.ts
+++ angular2/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   { path: 'angular2', component: AppComponent },
@@ -10,6 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
 })
 export class AppRoutingModule { }
